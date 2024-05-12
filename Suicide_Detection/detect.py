37d80b49a77caa3ee7d1662861f7007d5173f7aa
SAM <8dmasters@gmail.com>
2024-05-12 08:41:58 +0530

    Create detect.py
    Added main detection file

---
+++ Suicide_Detection/detect.py
@@ -0,0 +1,33 @@
+from ultralytics import YOLO
+from pathlib import Path
+
+import cv2
+
+
+def load_model():
+    '''Loads the YOLO model for detection'''
+
+    model_path = Path(__file__).resolve().parent / 'Model/Model_Data/logs/runs/detect/SuicideDetection/weights/best.pt'
+
+    model = YOLO(model_path)
+    return model
+
+
+def detect(model, image):
+    '''Detects objects in the image'''
+
+    image = cv2.resize(image, (640, 640))
+
+    results = model.predict(
+        source=image,
+        imgsz=(640, 640),
+        conf=0.5,
+        iou=0.7,
+        save=False,
+        save_txt=False,
+        show_conf=False,
+        show_labels=False,
+        show_boxes=False
+    )
+
+    return results
