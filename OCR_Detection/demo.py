662bff47435b7cf4600d11d42cf518b15dac7187
SAM <8dmasters@gmail.com>
2024-05-14 22:58:18 +0530

    Update demo.py
    Added full detected text display

--- OCR_Detection/demo.py
+++ OCR_Detection/demo.py
@@ -11,6 +11,8 @@ if __name__ == '__main__':
         image_data = detect.detect_text(frame)
         detected_text = detect.process_data(frame, image_data)
 
+        cv2.putText(frame, detected_text, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+
         cv2.imshow('Cam', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'): break # Exit when 'q' is pressed
